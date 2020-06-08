28ea8be95612baf3f41898013c00cd861d03f419
MrFMach <fabiosilvamachado@gmail.com>
2020-06-08 09:47:12 -0300

    Enviando html

--- index.js
+++ index.js
@@ -5,9 +5,32 @@ const srv = http.createServer((req, res) => {
     console.log('HEADERS: ', req.headers);
     console.log('HTTP VERSÃO: ', req.httpVersion);
 
-    res.end('Ola servidor HTTP!!');
+    res.writeHead(200, {    // retorna um status code
+        'Content-Type': 'text/html'           // tipo do conteúdo, no caso html
+    })
+
+    res.write(`
+    <!DOCTYPE html>
+            <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0>
+            <meta http-equiv="X-UA-Compatible" content="ie=edge">
+            <title>Fabio Machado</title>
+            <link rel="stylesheet" href="css\estilos.css">
+            <title>Servidor HTTP</title>
+        </head>
+        <body>
+            <h1>Ola servidor HTTP</h1>
+        <body>
+        </html>
+    `)
+
+    res.end()   // envia mensagem
+
 });
 
-srv.listen(3001, () => {
+
+srv.listen(3001, () => {        
     console.log('Servidor funcionando!');
 })
