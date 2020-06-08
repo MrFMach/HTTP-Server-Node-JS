f7ff7afbcb31d83e7e4af4ede7927f3da1a3ed60
MrFMach <fabiosilvamachado@gmail.com>
2020-06-08 17:28:03 -0300

    Adicionadas condições para  métodos POST e GET

--- index.js
+++ index.js
@@ -5,30 +5,37 @@ const srv = http.createServer((req, res) => {
     console.log('HEADERS: ', req.headers);
     console.log('HTTP VERSÃO: ', req.httpVersion);
 
-    res.writeHead(200, {    // retorna um status code
-        'Content-Type': 'text/html'           // tipo do conteúdo, no caso html
-    })
+    if (req.method === "POST")  {
+        res.writeHead(201, {
+            'Content-type': 'application/json'
+        })
+        res.write('{"message": "Cadastro realizado com sucesso"}')
+    }
 
-    res.write(`
-    <!DOCTYPE html>
-            <html lang="en">
-        <head>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0>
-            <meta http-equiv="X-UA-Compatible" content="ie=edge">
-            <title>Servidor HTTP</title>
-        </head>
-        <body>
-            <h1>Ola servidor HTTP</h1>
-        <body>
-        </html>
-    `)
+    if (req.method === "GET")   {
+        res.writeHead(200, {    
+        'Content-Type': 'text/html'
+        })
 
+        res.write(`
+        <!DOCTYPE html>
+                <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0>
+                <meta http-equiv="X-UA-Compatible" content="ie=edge">
+                <title>Servidor HTTP</title>
+            </head>
+            <body>
+                <h1>Ola servidor HTTP</h1>
+            <body>
+            </html>
+        `)
+    }
     res.end()   // envia mensagem
-
+    
 });
-
 
 srv.listen(3001, () => {        
     console.log('Servidor funcionando!');
-})
+});
